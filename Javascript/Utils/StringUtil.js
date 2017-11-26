bbd96d8493a4d4d0dcbea0ff2eb75d5a636b6aae
Unknown <664006332@qq.com>
2017-11-26 15:05:08 +0800

    添加注释

--- Javascript/Utils/StringUtil.js
+++ Javascript/Utils/StringUtil.js
@@ -4,8 +4,10 @@ function getStrLength(value) {
     value = (value + "").split("");
     for (var i = 0; i < value.length; i++) {
         var cn = value[i];
-        //全角字符校验，中文是全角，此处用的是UTF-8
-        var re = /[^\x00-\x80]/;//  /[^\x00-\x80]/; //或    /[^\0-\127]/    十进制表示
+        // 全角字符校验，中文是全角，此处用的是UTF-8
+		// 匹配双字节字符(包括汉字在内) /[^\x00-\x80]/ 或 /[^\0-\127]/ 
+        var re = /[^\x00-\x80]/;
+		// 匹配中文字符的正则表达式： [\u4e00-\u9fa5]
         if (/^[\u4e00-\u9fa5]+$/.test(cn) || re.test(cn)) {
             num = num + 3;
         } else {
